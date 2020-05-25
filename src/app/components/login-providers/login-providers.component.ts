21576fadac77d5905aaebd4ada836aa1c1bb0266
mcorverasoft <miltonwc2005@gmail.com>
2020-05-24 23:54:24 -0600

    fix some bugs of login

--- src/app/components/login-providers/login-providers.component.ts
+++ src/app/components/login-providers/login-providers.component.ts
@@ -86,7 +86,7 @@ export class LoginProvidersComponent implements OnInit {
           this.offProgress();
           console.log('ResponseLogin', this.responseLogin);
         },
-        (error) => {
+        (error:HttpErrorResponse) => {
                     this.handleError(error);
                     this.offProgress();
                   }
@@ -96,14 +96,18 @@ export class LoginProvidersComponent implements OnInit {
 
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
+       // A client-side or network error occurred. Handle it accordingly.
+       console.error('An error occurred:', error.error.message);
+       this.responseLogin.authenticated=false;
+       this.responseLogin.error=error.error.message;
     } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
-      );
+     // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        console.error(
+          'Backend returned code '+ error.status +', body was: '+error.error.message
+        );
+        this.responseLogin.authenticated=false;
+        this.responseLogin.error=error.error.message;
     }
     // return an observable with a user-facing error message
     return throwError('Something bad happened; please try again later.');
